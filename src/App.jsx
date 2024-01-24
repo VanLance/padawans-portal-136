9b1d42b3c88bb24f9ceaf11d25e3071cfb6df8dd
unknown <dylan23smith1990@gmail.com>
2024-01-23 20:05:41 -0600

    day2

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import Container from 'react-bootstrap/Container'
 
 import { Posts } from "./components/Posts"
 import Header from './components/Header'
+import Body from './components/Body'
+import Whiteboard from './components/Whiteboard'
+import Register from './components/forms/Register'
 
 export default function App() {
-
-
 
   return (
     <Container fluid data-bs-theme='dark' className='app'>
       <Header />
-
-      <Posts></Posts>
+      <Body>
+        {/* <Posts /> */}
+        {/* <Whiteboard /> */}
+        <Register />
+      </Body>
     </Container>
   )
 }
---
+++ src/components/Body.jsx
@@ -0,0 +1,18 @@
+import Container from 'react-bootstrap/Container'
+import Stack from 'react-bootstrap/Stack'
+
+import Sidebar from './Sidebar'
+
+export default function Body({ children }) {
+
+    return (
+        <Container>
+            <Stack direction='horizontal'>
+                <Sidebar />
+                <Container>
+                    { children }
+                </Container>
+            </Stack>
+        </Container>
+    )
+}
