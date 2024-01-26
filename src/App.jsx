9c85758581eb58415c734aa98fbc0ffd095dc70e
Brandt-C <brandt.carlson11@gmail.com>
2024-01-25 19:35:20 -0600

    ugly nested routing in

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Container from 'react-bootstrap/Container'
 
 import { Posts } from "./components/Posts"
 import Header from './components/Header'
-import Body from './components/Body'
 import Whiteboard from './components/Whiteboard'
 import Register from './components/forms/Register'
 import Users from './components/Users'
@@ -10,15 +9,22 @@ import FormPage from './pages/FormPage'
 import SocialPage from './pages/SocialPage'
 import LandingPage from './pages/LandingPage'
 import Login from './components/forms/Login'
+import { Route, Routes } from 'react-router-dom'
 
 export default function App() {
 
   return (
     <Container fluid data-bs-theme='dark' className='app'>
       <Header />
-      <FormPage>
-        <Login />
-      </FormPage> 
+
+      <Routes>
+        <Route path='/' element={<LandingPage><Whiteboard/></LandingPage>} />
+        <Route path='/login' element={<FormPage><Login /></FormPage>} />
+        <Route path='/register' element={<FormPage><Register/></FormPage>} />
+        <Route path='/users' element={<SocialPage><Users/></SocialPage>} />
+        <Route path='/posts' element={<SocialPage><Posts/></SocialPage>} />
+      </Routes>
+
     </Container>
   )
 }
